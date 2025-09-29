e803750eeca036203bff7b372b066b9f5501ff13
Lifo123 <alberto_p-@hotmail.com>
2025-09-29 15:26:08 -0500

    Update: Astro to 5.14.0.0 version

--- Astro/src/apps/Count.tsx
+++ Astro/src/apps/Count.tsx
@@ -6,8 +6,8 @@ export default function Count(props: any) {
     const [count, setCount] = useState(0);
 
     return (
-        <span className="btn btn-secondary br-6 d-flex f-center" onClick={() => setCount(count + 1)} style={{width: "120px"}}>
+        <button className="btn btn-third rounded-md d-flex f-center" onClick={() => setCount(count + 1)} style={{ width: "120px" }}>
             {props?.text} {count}
-        </span>    
+        </button>
     )
 }
